a2a295d7ea9d36755ff45c8baf9f8558f6ac25da
Mario Bodemann <mario.bodemann@yubico.com>
2026-01-09 10:48:10 +0100

    fix default test on fido-android

--- fido-android/src/androidTest/java/com/yubico/yubikit/fido/android/ExampleInstrumentedTest.kt
+++ fido-android/src/androidTest/java/com/yubico/yubikit/fido/android/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.yubico.yubikit.fido.android", appContext.packageName)
+        assertEquals("com.yubico.yubikit.fido.android.test", appContext.packageName)
     }
 }
